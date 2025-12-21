f591d71f97d014419da9091f7bc37f21e7f6829b
GEONHEEHAN <gksrjsgml13@naver.com>
2025-12-21 20:17:02 +0900

    스캔 옵선 수정(인식률)

--- Platforms/Android/ScanActivity.cs
+++ Platforms/Android/ScanActivity.cs
@@ -39,14 +39,22 @@ public class ScanActivity : Activity
                 ZXing.BarcodeFormat.CODE_128,
                 ZXing.BarcodeFormat.QR_CODE
             },
-            CameraResolutionSelector = new MobileBarcodeScanningOptions.CameraResolutionSelectorDelegate(SelectLowestResolutionMatchingDisplayAspectRatio),
+            CameraResolutionSelector = new MobileBarcodeScanningOptions.CameraResolutionSelectorDelegate(SelectHighestResolution),
             UseNativeScanning = true,
-            AutoRotate = false
+            AutoRotate = false,
+            TryHarder = true,
+            TryInverted = true,
+            PureBarcode = false,
+            DelayBetweenAnalyzingFrames = 3000,
+            InitialDelayBeforeAnalyzingFrames = 300
         };
 
         // 커스텀 오버레이
         _scanner.TopText = "바코드를 프레임 안에 맞춰주세요";
         _scanner.BottomText = "자동으로 인식됩니다";
+        _scanner.FlashButtonText = "플래시";
+
+        _scanner.UseCustomOverlay = false;
 
         // 스캔 시작 (카메라 화면이 자동으로 표시됨)
         var result = await _scanner.Scan(options);
@@ -67,7 +75,14 @@ public class ScanActivity : Activity
 
         Finish();
     }
-
+    
+    private CameraResolution SelectHighestResolution(List<CameraResolution> availableResolutions)
+    {
+        // 가장 높은 해상도 선택
+        return availableResolutions
+            .OrderByDescending(r => r.Width * r.Height)
+            .FirstOrDefault() ?? new CameraResolution { Width = 1920, Height = 1080 };
+    }
     private CameraResolution SelectLowestResolutionMatchingDisplayAspectRatio(List<CameraResolution> availableResolutions)
     {
         CameraResolution? result = null;
